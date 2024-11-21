a44d85a4748dd8fcfcded36841f157c7764e3861
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2024-11-21 13:13:19 -0500

    React and Boostrap, Main page

---
+++ 06.-Code/natulove/src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Carousel from './Components/Carousel';
+import Navbar from './Components/Navbar';
+
+// Componente HomePage
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+    </>
+  );
+};
+
+export default HomePage;
