d90bb824375aaf466ed5eb2c50a55fa73bd0758a
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2024-11-22 00:58:12 -0500

    V0.9

--- 06.-Code/natulove/src/HomePage.js
+++ 06.-Code/natulove/src/HomePage.js
@@ -2,10 +2,6 @@ import React from 'react';
 import Carousel from './Components/Carousel';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-<<<<<<< HEAD
-
-=======
->>>>>>> fa9d99c484f394a2a6a64c4e1077c57d47ff8065
 // Componente HomePage
 const HomePage = () => {
   return (
