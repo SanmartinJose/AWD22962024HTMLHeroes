ce47f7e46be867982a4909c6ac8a05941451cbfd
Matias Suarez Alexander Teran <masuarez18@espe.edu.ec>
2024-11-26 23:15:19 -0500

    Interview_summary_N2 V1.0.0

--- 06.-Code/natulove/src/App.js
+++ 06.-Code/natulove/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './HomePage';
+import Login from './Login';
 
 function App() {
   return (
-    <HomePage />    
+    <Login />    
   );
 }
 
