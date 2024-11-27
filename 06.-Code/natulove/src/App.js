c924100c2813c6d56b3dd1e507d9634d7d6b10b6
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2024-11-27 02:30:49 -0500

    Bugs Fixed V0.1.1

--- 06.-Code/natulove/src/App.js
+++ 06.-Code/natulove/src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './HomePage';
-<<<<<<< HEAD
-import NewProduct from './NewProduct';
-
-
-
-function App() {
-  return (
-    <HomePage	 />    
-    
-=======
 import Login from './Login';
 
 function App() {
   return (
-    <Login />    
->>>>>>> ce47f7e46be867982a4909c6ac8a05941451cbfd
+    <HomePage />    
   );
 }
 
