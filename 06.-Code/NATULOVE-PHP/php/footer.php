9f8f1b20d40e17071751d54d588cecc7d75fed20
Matias Suarez Alexander Teran <masuarez18@espe.edu.ec>
2024-11-30 20:11:00 -0500

    Product Description PHP V1.0.0

---
+++ 06.-Code/NATULOVE-PHP/php/footer.php
@@ -0,0 +1,34 @@
+<?php
+// Footer.php
+?>
+
+<footer class="footer bg-black text-light py-4">
+    <div class="container">
+        <div class="row">
+            <!-- Logo Section -->
+            <div class="col-md-2">
+                <img src="../../NATULOVE/imagenes/LogoTeam.png" alt="Develop Team Logo" class="img-fluid" />
+            </div>
+            <!-- Navigation Links -->
+            <div class="col-md-3">
+                <h5 class="text-uppercase">Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="/" class="footer-link">Home</a></li>
+                    <li><a href="/about" class="footer-link">About Us</a></li>
+                    <li><a href="/products" class="footer-link">Products</a></li>
+                    <li><a href="/contact" class="footer-link">Contact</a></li>
+                </ul>
+            </div>
+            <!-- Contact Info -->
+            <div class="col-md-6">
+                <h5 class="text-uppercase">Contact Us</h5>
+                <ul class="list-unstyled">
+                    <li>Email: <a href="mailto:support@natulove.com" class="footer-link">support@natulove.com</a></li>
+                    <li>Phone: +593 992536817</li>
+                    <li>Address: San Carlos, Quito, Ecuador</li>
+                </ul>
+            </div>
+        </div>
+        <small>&copy; 2024 Natulove. All Rights Reserved.</small>
+    </div>
+</footer>
