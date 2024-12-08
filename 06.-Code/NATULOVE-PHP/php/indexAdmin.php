4379c268458745a7cfbb4ec1d96a8bf8fdc858cf
SanmartinJose <132719688+SanmartinJose@users.noreply.github.com>
2024-12-08 16:25:08 -0500

    index fixed

--- 06.-Code/NATULOVE-PHP/php/indexAdmin.php
+++ 06.-Code/NATULOVE-PHP/php/indexAdmin.php
@@ -57,10 +57,6 @@
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> d3c24a7d102756dc1d6c96d36b39ae276f490afb
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <script src="./scriptsJs/login.js"></script>
 </body>
