ebbbf24f947b3ae7196a00247fd165d034a07991
Justin Villarroel <jjvillarroel1@espe.edu.ec>
2025-01-23 08:23:07 -0500

    Fixing WHAP api

--- 06.-Code/NatuloveStore/php/aboutUs.php
+++ 06.-Code/NatuloveStore/php/aboutUs.php
@@ -45,25 +45,6 @@
             </div>
         </div>
     </div>
-    <div class="col-md-3"></div>
-	<div class="col-md-6 well" style="background-color: #0BA9FD">
-		<h3 class="text-primary">Display Google Map Data</h3>
-		<hr style="border-top:1px dotted #ccc;"/>
-		<button class="btn btn-primary" id="get_map"><span class="glyphicon glyphicon-location"></span> Load Map</button>
-		<div id="map" style="height:500px; display:none;"></div>
-	</div>
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-AB-9XZd-iQby-bNLYPFyb0pR2Qw3orw&callback=initMap" async defer></script>	
-	<script>
-		var map;
-		function initMap() {
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: {lat: 9.5162, lng: 123.158},
-				zoom: 8
-			});
-		}
-	</script>
-	<script src="js/script.js"></script>
 
     
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
